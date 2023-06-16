0258f54a449c11135100fe83c2449646172302c8
voste126 <steveaustine126@gmail.com>
2023-06-16 17:16:19 +0300

    my DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const removeMain = document.querySelector('main');
+removeMain.remove();
+// Declare the newHeader variable
+let newHeader;
+//create the h1
+newHeader = document.createElement('h1')
+//use the variable name to give the header an id
+newHeader.id = 'victory'
+//use textcontent to add content to the header
+newHeader.textContent = "YOUR-NAME is the champion" ;
+
